37ec01352efad1aae18265452a2d7af4c9f2e49f
Pek Ratanak <r.pek@gl-f.com>
2018-04-10 09:50:36 +0700

    Test

---
+++ server.js
@@ -0,0 +1,55 @@
+/**
+ * Created by r.pek on 4/10/2018.
+ */
+var express = require('express');
+var app = express();
+var PORT = 8888;
+
+var todos = [
+    {
+        id: 1,
+        name:'Pek ratanak',
+        description:'He is handsome!'
+    },
+    {
+        id:2,
+        name:'Hum Vorn',
+        description:'He is fat like a pig!'
+    },
+    {
+        id:3,
+        name:'Heim Seyha',
+        description:'He is thin!'
+    }
+];
+
+
+app.get('/', function (req, res) {
+    res.send('Hello World');
+});
+
+
+app.get('/json', function(req, res){
+    res.json(todos);
+});
+
+app.get('/todos/:id', function(req, res){
+    var id=parseInt(req.params.id, 10);
+    var matched;
+    todos.forEach(function(item){
+        if(id===item.id){
+            matched=item;
+        }
+   });
+
+   if(matched){
+       res.json((matched));
+   }else{
+       res.status(404).send();
+   }
+
+});
+
+app.listen(PORT, function () {
+    console.log('Listen on port : ' + PORT);
+});
